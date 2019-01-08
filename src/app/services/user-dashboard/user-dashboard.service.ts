fceff0271136853d9aa8f58d42e6386793d4567a
Sonal <sona.karma21@gmail.com>
2019-01-08 21:57:13 +0530

    ncm/user dash icon added/modified

--- src/app/services/user-dashboard/user-dashboard.service.ts
+++ src/app/services/user-dashboard/user-dashboard.service.ts
@@ -9,10 +9,10 @@ const data: UserDashboardData = {
 		{    
 			"workRequestID" : "R001",
 			"workOrderID" : "A001",
-			"typeOfWork" : "contractual",
+			"typeOfWork" : "Contractual",
 			"project" :"Sound of water",
-			"package" : "pillars",
-			"needByDate" : "30/01/2019",
+			"package" : "Structural",
+			"needByDate" : "15/02/2019",
 		    "workCategory": "electric",
 	      	"supportRole" : "enginneer",
 	      	"assignee" : "technicain",
@@ -25,13 +25,13 @@ const data: UserDashboardData = {
 		    "status": "Pending",
 	      	"documentStatus" : "Pending",
 	      	"remarks" : "Good",
-		}
+		},
 		{    
 			"workRequestID" : "R002",
 			"workOrderID" : "A002",
-			"typeOfWork" : "contractual",
-			"project" :"Sound of water",
-			"package" : "pillars",
+			"typeOfWork" : "Excavation",
+			"project" :"The Tree",
+			"package" : "Finshing",
 			"needByDate" : "30/01/2019",
 		    "workCategory": "electric",
 	      	"supportRole" : "enginneer",
@@ -45,14 +45,34 @@ const data: UserDashboardData = {
 		    "status": "Pending",
 	      	"documentStatus" : "Pending",
 	      	"remarks" : "Good",
-		}
+		},
 		{    
 			"workRequestID" : "R003",
-			"workOrderID" : "A003",
-			"typeOfWork" : "contractual",
-			"project" :"Sound of water",
-			"package" : "pillars",
-			"needByDate" : "30/01/2019",
+			"workOrderID" : "A004",
+			"typeOfWork" : "Consulting",
+			"project" :"Plan Beach",
+			"package" : "Design Work",
+			"needByDate" : "05/02/2019",
+		    "workCategory": "electric",
+	      	"supportRole" : "enginneer",
+	      	"assignee" : "technicain",
+	      	"putintiateDate" : "10/10/2017",
+		    "RFAPutDate" : "10/10/2017",
+		    "WOPutDate" : "10/10/2017",
+		    "approvalintiateDate" : "20/10/2017",
+		    "RFAApprovalDate" : "16/10/2017",
+		    "WOApprovalDate" : "26/10/2017",
+		    "status": "Pending",
+	      	"documentStatus" : "Pending",
+	      	"remarks" : "Good",
+		},
+		{    
+			"workRequestID" : "R004",
+			"workOrderID" : "A004",
+			"typeOfWork" : "Architectural",
+			"project" :"The park",
+			"package" : "Excavation",
+			"needByDate" : "10/02/2019",
 		    "workCategory": "electric",
 	      	"supportRole" : "enginneer",
 	      	"assignee" : "technicain",
@@ -66,6 +86,8 @@ const data: UserDashboardData = {
 	      	"documentStatus" : "Pending",
 	      	"remarks" : "Good",
 		}
+		
+
 	]
 }
 
