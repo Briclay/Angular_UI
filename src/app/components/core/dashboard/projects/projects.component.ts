0e33b7be88949e6578e29b624ab653da8ecdc409
Mahamed Zubair <mahamed@nestlabs.com>
2019-02-01 01:49:34 +0530

    Project UI Styling fixes

--- src/app/components/core/dashboard/projects/projects.component.ts
+++ src/app/components/core/dashboard/projects/projects.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ProjectService} from './project.service'
+import { ProjectService } from './project.service'
 import { MatDialog, MatTableDataSource } from '@angular/material';
 
 @Component({
@@ -13,29 +13,50 @@ export class ProjectsComponent implements OnInit {
   projects: any = {
     data: []
   };
-  orgId:string;
+  orgId: string;
   projectLoading: boolean;
-  displayedColumns: string[] = ['name','projectCode','budget','toatlUnit'];
-  constructor(private projectService: ProjectService) { 
-    var org=JSON.parse(window.localStorage.authUserOrganisation);
+  displayedColumns: string[] = ['name', 'projectCode', 'budget', 'toatlUnit'];
+  constructor(private projectService: ProjectService) {
+    var org = JSON.parse(window.localStorage.authUserOrganisation);
     this.orgId = org._id;
     this.getProjects();
   }
 
   ngOnInit() {
-   // this.getProjects();
+    // this.getProjects();
   }
 
   getProjects() {
-    this.projectService.getProjects( this.orgId).pipe().subscribe(res => {
-      console.log('res',res);
+    this.projectService.getProjects(this.orgId).pipe().subscribe(res => {
+      console.log('res', res);
       this.projectLoading = false;
+      res.forEach((list) => { 
+        list.displayLogo = list.imageUrls[0];
+        list.unitNumber = list.units.reduce((accumulator, currentValue) => {
+            return accumulator + currentValue.count;
+        }, 0);
+    })
       this.projects = res;
+      this.projectDataOptions = [
+                {
+          title: 'Image', key: 'displayLogo', hideTitle: true, type: 'image'
+        },
+        {
+          title: 'User Name', type: 'list', list: [
+            { title: 'UserName', key: 'name', hideTitle: true, type: 'label' },
+            { title: 'Address', key: 'projectDetails.location', hideTitle: true, type: 'label' },
+            { title: 'Address', key: 'status', hideTitle: true, type: 'label', isStatus: true }
+          ]
+        },
+        { title: 'Project Code', key: 'projectCode' },
+        { title: 'Total Units', key: 'unitNumber' },
+        { title: 'Budget', key: 'projectDetails.unitNumber' }]
+
       this.dataSource = new MatTableDataSource(res);
     }, (error: any) => {
-        console.error('error', error);
-        this.projectLoading = false;
-      });
+      console.error('error', error);
+      this.projectLoading = false;
+    });
   }
   tabSwitch(index) {
     this.getProjects();
--- src/app/components/shared/expansion-panel/expansion-panel.component.ts
+++ src/app/components/shared/expansion-panel/expansion-panel.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 })
 export class ExpansionPanelComponent implements OnInit {
   @Input() expansionPanelOptions: any;
+  @Input() isDisabled: boolean;
   @Input() data: any; 
   @Input() selectorComponent: any;
   @Input() isHistory: boolean;
