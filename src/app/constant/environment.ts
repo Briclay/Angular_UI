d2caf16a1ea83c15afffd9777b6b8b7f682a368c
mohitmaddhesiya <mohitmaddhesiya@gmail.com>
2019-02-12 20:15:19 +0530

    changed endpoint of work request

--- src/app/constant/environment.ts
+++ src/app/constant/environment.ts
@@ -3,7 +3,7 @@ const API_BASE = "api/dashboards/v1";
 const BRICLAY_CORE = "http://localhost:8099";*/
 const BRICLAY_FILE_MANAGER = "http://13.232.182.5:90";
 const BRICLAY_CORE = "http://13.232.182.5";
-const BRICLAY_WORK_REQUSEST = "https://briclay-work-tracker.herokuapp.com"
+const BRICLAY_WORK_REQUSEST = "http://13.232.182.5:91"
 
 export const environmentService = {
   briclayFileManager: BRICLAY_FILE_MANAGER,
